5f00240e3b8b9f0d8ed595472fbc4d9f0aee8479
Milan <mdasgup2@illinois.edu>
2014-09-27 23:13:20 -0500

    Initial Commit

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "worldstar.h"
+
+int isthree(int x);
+
+int main()
+{
+	int var = 3;
+	var turnup;
+	var turndown;
+	if (isthree(var))
+		printf("Hello World\n");
+	worldstar;
+}
+
+int isthree(int x)
+{
+	if (x==3)
+		return TRU;
+	else
+		return NOOO;
+}
+
+
